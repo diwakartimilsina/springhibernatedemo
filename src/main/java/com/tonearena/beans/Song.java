ad01990dcd6e9f310e2d494b5cd3ac0826d03afc
U-diwakarpc-PC\diwakarpc <diwakarpc@diwakarpc-PC.(none)>
2014-06-24 17:05:21 -0500

    initial commit

---
+++ src/main/java/com/tonearena/beans/Song.java
@@ -0,0 +1,20 @@
+package com.tonearena.beans;
+
+public class Song {
+
+	int songId;
+	String songName;
+	
+	public int getSongId() {
+		return songId;
+	}
+	public void setSongId(int songId) {
+		this.songId = songId;
+	}
+	public String getSongName() {
+		return songName;
+	}
+	public void setSongName(String songName) {
+		this.songName = songName;
+	}
+}
